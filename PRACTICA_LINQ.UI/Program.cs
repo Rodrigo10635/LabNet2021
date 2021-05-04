ca2a7f60969ba81a988c08be0fe0e7c9068e8a8c
Rodrigo10635 <rodrigo.botana@hotmail.com>
2021-05-03 22:44:01 -0300

    Subo trabajo de LINQ

---
+++ PRACTICA_LINQ.UI/Program.cs
@@ -0,0 +1,212 @@
+﻿using PRACTICA_LINQ.Data;
+using PRACTICA_LINQ.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PRACTICA_LINQ.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new NorthwindContext();
+
+            var EJERCICIO_1 = context.Customers; 
+
+            foreach (var item in EJERCICIO_1)
+            {
+                Console.WriteLine($"{item.CustomerID}-{item.CompanyName}-{item.Address}-{item.City}-{item.Country}-{item.Phone}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadLine();
+
+            var EJERCICIO_2 = context.Products.Where(p => p.UnitsInStock == 0); 
+
+            foreach (var item in EJERCICIO_2)
+            {
+                Console.WriteLine($"{item.ProductID}-{item.ProductName}-{item.UnitsInStock}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadLine();
+
+            var EJERCICIO_3 = from producto in context.Products //OK
+                         where (producto.UnitsInStock != 0) && (producto.UnitPrice > 3)
+                         select producto;
+
+            foreach (var item in EJERCICIO_3)
+            {
+                Console.WriteLine($"{item.ProductID}-{item.ProductName}-{item.UnitsInStock}");
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadLine();
+
+            var EJERCICIO_4 = from clientes in context.Customers //OK
+                               where clientes.Region == "WA"
+                               select clientes;
+
+            foreach (var item in EJERCICIO_4)
+            {
+                Console.WriteLine($"{item.CustomerID}-{item.CompanyName}-{item.Address}-{item.City}-{item.Country}-{item.Phone}");
+            }
+           
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadLine();
+
+            var EJERCICIO_5 = context.Products.Where(p => p.ProductID == 789).FirstOrDefault();
+
+            if (EJERCICIO_5 != null)
+            {
+                Console.WriteLine($"{EJERCICIO_5.ProductName}");
+            }
+            else
+                Console.WriteLine("El valor es nulo");
+
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadLine();
+
+            var EJERCICIO_6 = context.Products;
+
+            foreach (var item in EJERCICIO_6)
+            {
+                Console.WriteLine(item.ProductName.ToLower());
+            }
+
+            foreach (var item in EJERCICIO_6)
+            {
+                Console.WriteLine(item.ProductName.ToUpper());
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadLine();
+
+            DateTime fecha = DateTime.Parse("1/1/1997");
+            var EJERCICIO_7 = context.Customers.Join(context.Orders,
+
+                cl => cl.CustomerID,
+                ord => ord.CustomerID,
+                (cl, ord) => new
+                {
+                    cl.CustomerID,
+                    ord.OrderDate,
+                    cl.Region
+                }).Where(cl => cl.Region == "WA" && cl.OrderDate > fecha);
+
+            foreach (var item in EJERCICIO_7)
+            {
+                Console.WriteLine($"{item.CustomerID}-{item.OrderDate}-{item.Region}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadLine();
+
+            var EJERCICIO_8 = context.Customers.Where(C => C.Region == "WA").Take(3); //OK
+
+
+            foreach (var item in EJERCICIO_8)
+            {
+                Console.WriteLine($"{item.CustomerID}-{item.CompanyName}-{item.Address}-{item.City}-{item.Country}-{item.Phone}");
+            }
+           
+
+            
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadLine();
+
+            var EJERCICIO_9 = from productos in context.Products
+                              orderby productos.ProductName
+                              select productos;
+           
+            foreach (var item in EJERCICIO_9)
+            {
+                Console.WriteLine($"{item.ProductID}-{item.ProductName}-{item.UnitsInStock}");
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadLine();
+
+            var EJERCICIO_10 = from productos in context.Products
+                               orderby productos.UnitsInStock descending
+                               select productos;
+
+            foreach (var item in EJERCICIO_10)
+            {
+                Console.WriteLine($"{item.ProductID}-{item.ProductName}-{item.UnitsInStock}");
+
+            }
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadLine();
+
+            var EJERCICIO_11 = context.Products.Join(context.Categories,
+                sc => sc.CategoryID,
+                soc => soc.CategoryID,
+                (sc, soc) => new
+                {
+                    sc.ProductID,
+                    soc.CategoryName
+                }).GroupBy(g => g.CategoryName).ToList();
+
+         
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadLine();
+
+            var EJERCICIO_12 = context.Products.First();
+
+
+            Console.WriteLine($"el nombre del primer producto es {EJERCICIO_12.ProductName}");
+            
+
+            Console.WriteLine();
+            Console.WriteLine();
+            Console.ReadLine();
+
+            var EJERCICIO_13 = from p in context.Orders.GroupBy(p => p.CustomerID) 
+                          select new
+                          {
+                              count = p.Count(),
+                              p.FirstOrDefault().CustomerID,
+                          };
+
+            
+
+
+            foreach (var item in EJERCICIO_13)
+            {
+                Console.WriteLine($"{item.CustomerID } { item.count}");
+            }
+
+            Console.ReadLine();
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}
---
+++ PRACTICA_LINQ.UI/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// La información general de un ensamblado se controla mediante el siguiente 
+// conjunto de atributos. Cambie estos valores de atributo para modificar la información
+// asociada a un ensamblado.
+[assembly: AssemblyTitle("PRACTICA_LINQ.UI")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("PRACTICA_LINQ.UI")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Si establece ComVisible en false, los tipos de este ensamblado no estarán visibles 
+// para los componentes COM.  Si es necesario obtener acceso a un tipo en este ensamblado desde 
+// COM, establezca el atributo ComVisible en true en este tipo.
+[assembly: ComVisible(false)]
+
+// El siguiente GUID sirve como id. de typelib si este proyecto se expone a COM.
+[assembly: Guid("da24a491-11b2-4c88-a1e8-1e2e2c5d399c")]
+
+// La información de versión de un ensamblado consta de los cuatro valores siguientes:
+//
+//      Versión principal
+//      Versión secundaria
+//      Número de compilación
+//      Revisión
+//
+// Puede especificar todos los valores o usar los valores predeterminados de número de compilación y de revisión
+// utilizando el carácter "*", como se muestra a continuación:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
